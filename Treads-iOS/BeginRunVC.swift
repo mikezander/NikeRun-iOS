eef14a8b95b9c707571feca8be8e00808b6df174
mikezander <mikezander87@yahoo.com>
2017-12-12 14:58:33 -0500

    StartRunVC UI complete

--- Treads-iOS/BeginRunVC.swift
+++ Treads-iOS/BeginRunVC.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  BeginRunVC.swift
 //  Treads-iOS
 //
 //  Created by Michael Alexander on 12/12/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class BeginRunVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
